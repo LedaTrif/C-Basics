92bd54d0ac037ece0b936ca84702ed3a934082bb
Leda <leda.trifonova@gmail.com>
2022-10-15 11:37:33 +0200

    invalid Number

---
+++ invalidNumber.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _08InvalidNUmber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            bool isValid = (number >= 100 && number <= 200 || number == 0);
+
+            if (!isValid)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
+
