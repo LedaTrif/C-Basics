e6fe4f052b20c7e0f58b094589129dc793d772fa
Leda <leda.trifonova@gmail.com>
2022-10-25 06:10:50 +0200

    Even Math Pow

---
+++ mathPow.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace E04EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i+=2)
+            {
+                Console.WriteLine(Math.Pow(2,i));
+            }
+        }
+    }
+}
+
