cc580f34336ac89a78a0907dd32d736d77bcd18d
Leda <leda.trifonova@gmail.com>
2022-10-29 10:25:39 +0200

    While Sequence 2k+1

---
+++ sequence2kPlus1.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace _04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int k = 1;
+
+            while (k<=num)
+            {
+                Console.WriteLine(k);
+
+                k = k * 2 + 1;
+
+            }
+
+        }
+    }
+}
+
