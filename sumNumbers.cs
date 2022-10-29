2ae7e7717d312ef363ca56ae544a007d6b26c15a
Leda <leda.trifonova@gmail.com>
2022-10-29 10:18:55 +0200

    While Sum Numbers

---
+++ sumNumbers.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace _03NumberSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (sum < number)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                sum += currentNumber;
+
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
+
