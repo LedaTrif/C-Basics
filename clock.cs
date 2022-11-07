19984bab2f73dc2a6b68bd866777573237aa60e8
Leda <113000102+LedaTrif@users.noreply.github.com>
2022-11-07 05:22:18 +0100

    Nested loop Clock

---
+++ clock.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour <=23; hour++)
+            {
+                for (int minutes = 0; minutes <=59; minutes++)
+                {
+                    Console.WriteLine($"{hour}:{minutes}");
+                }
+            }
+        }
+    }
+}
+
